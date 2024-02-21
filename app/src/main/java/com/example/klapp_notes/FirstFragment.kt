865d17955487dd0db1ece5294706d8695d644022
Raduuuk <radomir.pestov1@yandex.ru>
2024-02-21 12:58:01 +0200

    alert window added

--- app/src/main/java/com/example/klapp_notes/FirstFragment.kt
+++ app/src/main/java/com/example/klapp_notes/FirstFragment.kt
@@ -1,40 +1,49 @@
 package com.example.klapp_notes
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.example.klapp_notes.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
-/* hui */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            // Create an AlertDialog.Builder
+            val alertDialogBuilder = AlertDialog.Builder(requireContext())
+
+            // Set the title and message for the dialog
+            alertDialogBuilder.setTitle("Add new work space")
+            alertDialogBuilder.setMessage("write your title")
+
+            // Set positive button and its click listener
+            alertDialogBuilder.setPositiveButton("+") { dialog, which ->
+                // Perform the action when OK button is clicked
+                // For example, you can close the dialog or navigate to another fragment
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
+
+            // Create and show the alert dialog
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
         }
     }
 
