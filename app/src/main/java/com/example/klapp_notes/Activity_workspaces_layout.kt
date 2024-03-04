a7248bce2e272f93b1f3fdc0d07dc9a757e9a89d
Raduuuk <radomir.pestov1@yandex.ru>
2024-03-04 12:01:41 +0200

    add table

--- app/src/main/java/com/example/klapp_notes/Activity_workspaces_layout.kt
+++ app/src/main/java/com/example/klapp_notes/Activity_workspaces_layout.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
@@ -20,10 +21,19 @@ class Activity_workspaces_layout : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val buttonAddTable = view.findViewById<Button>(R.id.buttonAddTable)
-        buttonAddTable.setOnClickListener {
+        val buttonAddNote = view.findViewById<Button>(R.id.buttonAddNote)
+        buttonAddNote.setOnClickListener {
             // Navigate to the ActivityNote fragment
             findNavController().navigate(R.id.action_workspace_layout_to_ActivityNote)
         }
+
+        val buttonAddTable = view.findViewById<Button>(R.id.buttonAddTable)
+        val tableSquare = view.findViewById<FrameLayout>(R.id.tableSquare)
+
+        buttonAddTable.setOnClickListener {
+            // Set the visibility of the tableSquare to VISIBLE
+            tableSquare.visibility = View.VISIBLE
+        }
+
     }
 }
