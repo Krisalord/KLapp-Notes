ae1d4953b9d53c0c3d5dafa23b31daeeb1632595
Raduuuk <radomir.pestov1@yandex.ru>
2024-03-20 12:22:41 +0200

    table

---
+++ app/src/main/java/com/example/klapp_notes/TableDao.kt
@@ -0,0 +1,13 @@
+package com.example.klapp_notes
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+@Dao
+interface TableDao {
+    @Insert
+    suspend fun insertTable(table: Table)
+
+    @Query("SELECT * FROM tables WHERE workspace_id = :workspaceId")
+    suspend fun getTablesByWorkspace(workspaceId: Int): List<Table>
+}
