e336418a184917dd8266d32b4e517138d4f85fd7
Artur Motin <artur.motin98@gmail.com>
2024-04-03 12:28:28 +0300

    Tables
    Adding tables with workspace's ID

--- app/src/main/java/com/example/klapp_notes/WorkspaceDao.kt
+++ app/src/main/java/com/example/klapp_notes/WorkspaceDao.kt
@@ -7,7 +7,7 @@ import androidx.room.Query
 @Dao
 interface WorkspaceDao {
     @Insert
-    suspend fun insert(workspace: Workspace)
+    suspend fun insert(workspace: Workspace): Long
 
     @Query("SELECT * FROM workspaces")
     suspend fun getAllWorkspaces(): List<Workspace>
